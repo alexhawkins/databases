c53a8a5838a7d7a9205fa11913255c5e77c03d7f
alexhawkins <alexhawkins.me@gmail.com>
2014-11-21 16:59:01 -0800

    Implement schema.db

--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,30 @@
 CREATE DATABASE chat;
-
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS rooms;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  user_id INTEGER NULL DEFAULT NULL,
+  room_id INTEGER NULL DEFAULT NULL,
+  text VARCHAR(150) NULL DEFAULT NULL,
+  created_at DATETIME NULL DEFAULT NOW(),
+  PRIMARY KEY (id)
 );
 
+CREATE TABLE users (
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  name VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  name VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 /* Create other tables and define schemas for them here! */
 
 
